a707e893022b621ba9cdc12581ed58ac0ec69c1f
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2024-07-30 19:13:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -28,3 +28,11 @@ const perguntas = [
         ]
     }  
 ];
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta();
