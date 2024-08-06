29c0dc8a0d45d12222a720386229d7cbe5c2d2fd
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2024-08-06 14:07:52 -0300

    subi arquivos JS

--- script.js
+++ script.js
@@ -4,30 +4,55 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
+
 const perguntas = [
     {
         enunciado: "O que fazer quando  identificar que uma criança está sofrendo bullying?",
-        alternativas: [
-            "Reportar imediatamente à equipe escolar e oferecer apoio emocional, garantindo que o aluno saiba que tem suporte e não está sozinho.", 
-            "Intervir e defender a vítima, conversar com o agressor."
+        alternativas: [ 
+            {
+                texto:  "Reportar imediatamente à equipe escolar e oferecer apoio emocional, garantindo que o aluno saiba que tem suporte e não está sozinho.",
+                afirmacao: "afirmação 1"
+            },
+            {
+                texto:   "Intervir e defender a vítima, conversar com o agressor.",
+                afirmacao: "afirmação 2"
+            }    
+           
         ]
     },
     {
         enunciado: "A prática regular  de um esporte melhora  o condicionamento físico e a socialização. Você pratica algum esporte regularmente?",
         alternativas: [
-            "Eu não pratico esportes da maneira convencional, mas amo o esporte como meio para socialização.",
- 
-            "O esporte é uma atividade competitiva que tem regras fixas e treinamentos e pratico regularmente."
+            {
+                texto:  "Eu não pratico esportes da maneira convencional, mas amo o esporte como meio para socialização.",
+                afirmacao: "afirmação 1"
+            },
+            {
+                texto:  "O esporte é uma atividade competitiva que tem regras fixas e treinamentos e pratico regularmente.",
+                afirmacao: "afirmação 2"
+            }   
+            
         ]
     },
     {
         enunciado: "A energia solar é uma fonte de energia renovável e sustentável que é obtida a partir da luz do sol. Diante disso eu:",
         alternativas: [
-            "Investiria em energia solar", 
-            "Iria dar continuade ao uso da energia hidrelétrica"
+            {
+                texto:  "Investiria em energia solar",  
+                afirmacao: "afirmação 1"
+            },
+            {
+                texto:    "Iria dar continuade ao uso da energia hidrelétrica",
+                afirmacao: "afirmação 2"
+            }             
+           
         ]
     }  
 ];
+
+
+         
+ 
 let atual = 0;
 let perguntaAtual;
 
@@ -37,11 +62,18 @@ function mostraPergunta(){
     caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
+
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostraPergunta();
+        });
+
         caixaAlternativas.appendChild(botaoAlternativas);
+
     }
 
 }
